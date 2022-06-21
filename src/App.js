537e31f70556bbf8a15e571cd78cf33145184d32
goranaviani <goranaviani@gmail.com>
2022-06-21 21:26:07 +0200

    rework

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ import './App.css';
 import axios from "axios";
 
 function App() {
-    const [movies, setMovies ] = useState([])
+    const [movies, setMovies] = useState([])
     const [isLoading, setIsLoading] = useState(false)
 
     const fetchMoviesHandler = async () => {
-setIsLoading (true)
-   const result = await axios({
-        method: "get",
-        url: 'https://swapi.dev/api/films/'
-   })
-        console.log( result.data.results)
+        setIsLoading(true)
+        const result = await axios({
+            method: "get",
+            url: 'https://swapi.dev/api/films/'
+        })
+        console.log(result.data.results)
 
         const processedResult = result.data.results.map(
             (row) => {
                 return {
-                        id: row.episode_id,
-                        title: row.title,
-                        releaseDate: row.release_date,
-                        openingText: row.opening_crawl,
+                    id: row.episode_id,
+                    title: row.title,
+                    releaseDate: row.release_date,
+                    openingText: row.opening_crawl,
                 }
             }
         )
@@ -31,19 +31,22 @@ setIsLoading (true)
         setIsLoading(false)
     }
 
-  return (
-    <React.Fragment>
-      <section>
-        <button onClick={() => {fetchMoviesHandler()}}>Fetch Movies</button>
-      </section>
-      <section>
-          {isLoading && <p>Loading...</p>}
-          {!isLoading && movies.length > 0 &&  <MoviesList movies={movies} /> }
-            {!isLoading && movies.length === 0 &&  <p>No movies found</p> }
+    return (
+        <React.Fragment>
+            <section>
+                <button onClick={() => {
+                    fetchMoviesHandler()
+                }}>Fetch Movies
+                </button>
+            </section>
+            <section>
+                {isLoading && <p>Loading...</p>}
+                {!isLoading && movies.length > 0 && <MoviesList movies={movies}/>}
+                {!isLoading && movies.length === 0 && <p>No movies found</p>}
 
-      </section>
-    </React.Fragment>
-  );
+            </section>
+        </React.Fragment>
+    );
 }
 
 export default App;
